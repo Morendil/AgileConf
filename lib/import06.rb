bd7b04ef880afeb6f0f0ee64e6cf1c707f783236
Laurent Bossavit <laurent.git@bossavit.com>
2012-06-21 10:21:48 +0200

    Import 2006 data

---
+++ lib/import06.rb
@@ -0,0 +1,16 @@
+require 'dm-core'
+require 'dm-migrations'
+
+require "./lib/session.rb"
+require "./lib/speaker.rb"
+require "./lib/parser06.rb"
+
+DataMapper.setup(:default, ENV['DATABASE_URL'])
+DataMapper.auto_upgrade!
+
+parser = Parser06.new "data/Agile2006.csv"
+begin
+  session = Session.from parser.hash
+  session.year = 2006
+  session.save
+end while parser.shift
