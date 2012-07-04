9da70ce2e76b191c5217adc1130495683cd4b338
Laurent Bossavit <laurent.git@bossavit.com>
2012-07-04 15:49:06 +0200

    Fix overly narrow String fields, changed to Text

--- lib/video.rb
+++ lib/video.rb
@@ -4,8 +4,8 @@ class Video
   include DataMapper::Resource
 
   property :id, Serial, :key => true
-  property :thumb, String
-  property :media, String
+  property :thumb, Text
+  property :media, Text
   property :player, String
   property :width, Integer
   property :height, Integer
